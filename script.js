93b6ed30383596e4bfd2a9c8701060775ada30e3
Maxime1406 <promaxbardeau@gmail.com>
2026-03-29 21:12:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-// --- LES STOCKS XXL ---
+// --- LES PROTEINES (35 options) ---
 const proteines = [
     "Steak haché", "Pavé de saumon", "Filet de poulet", "Saucisses", "Filet de lieu", 
     "Côte de porc", "Jambon blanc", "Omelette", "Boulettes de bœuf", "Cordon bleu", 
     "Magret de canard", "Escalope de veau", "Crevettes", "Œufs au plat", "Thon en boîte", 
     "Aiguillettes de canard", "Poisson pané", "Nuggets maison", "Tranche de gigot", 
-    "Pavé de cabillaud", "Bacon grillé", "Dés de jambon cru",
-    "Boudin noir", "Aile de raie", "Brochettes de dinde", "Filet mignon"
+    "Pavé de cabillaud", "Sardines à l'huile", "Bacon grillé", "Dés de jambon cru",
+    "Boudin noir", "Aile de raie", "Brochettes de dinde", "Filet mignon", "Pavé de thon",
+    "Foie de veau", "Saucisse de Morteau", "Quenelles de brochet", "Escalope de dinde",
+    "Colin d'Alaska", "Cuisses de poulet", "Rôti de porc froid"
 ];
 
+// --- LES FECULENTS (30 options) ---
 const feculents = [
     "Pâtes", "Riz blanc", "Pommes de terre sautées", "Purée maison", "Lentilles", 
     "Coquillettes", "Semoule", "Frites", "Quinoa", "Petits pois", "Blé (Ebly)", 
     "Gratin dauphinois", "Riz pilaf", "Gnocchis", "Polenta", "Boulgour", "Pommes de terre à l'eau",
-    "Penne au beurre", "Tagliatelles", "Purée de patate douce", "Haricots blancs", "Riz cantonais"
+    "Penne au beurre", "Tagliatelles", "Purée de patate douce", "Haricots blancs", "Riz cantonais",
+    "Croquettes de P.D.T", "Tagliatelles fraîches", "Risotto nature", "Millet", "Pâtes complètes",
+    "Tortellini", "Pomme au four", "Riz sauvage"
 ];
 
+// --- LES LEGUMES (35 options) ---
 const legumes = [
     "Haricots verts", "Épinards", "Ratatouille", "Salade verte", "Carottes", 
     "Chou-fleur", "Brocolis", "Poivrons sautés", "Courgettes", "Fondue de poireaux", 
     "Petits légumes vapeur", "Tomates provençales", "Champignons de Paris", 
     "Purée de potiron", "Aubergines grillées", "Salade de tomates", "Poêlée paysanne",
-    "Asperges", "Céleri branche", "Choux de Bruxelles", "Endives braisées"
+    "Asperges", "Céleri branche", "Choux de Bruxelles", "Endives braisées", "Chou rouge",
+    "Betteraves", "Purée de panais", "Fenouil sauté", "Pois gourmands", "Haricots plats",
+    "Chou kale", "Poêlée de champignons", "Artichaut", "Radis", "Salade de concombre",
+    "Épinards frais", "Mélange de légumes oubliés", "Velouté de légumes"
 ];
 
 const platsMijotes = [
@@ -28,8 +37,9 @@ const platsMijotes = [
     "Couscous", "Paëlla", "Tartiflette", "Pot-au-feu", "Cassoulet", "Moussaka", 
     "Chili con carne", "Cannellonis", "Gratin de pâtes", "Poulet Basquaise", 
     "Osso Buco", "Carbonnade flamande", "Risotto aux champignons", "Petit salé aux lentilles",
-    "Veau Marengo", "Quiche aux poireaux", "Endives au jambon", "Petit salé aux lentilles",
-    "Rougail saucisse", "Navarin d'agneau", "Tajine de poulet"
+    "Veau Marengo", "Quiche aux poireaux", "Endives au jambon", "Rougail saucisse", 
+    "Navarin d'agneau", "Tajine de poulet", "Choucroute", "Pieds de porc", 
+    "Lapin à la moutarde", "Axoa de veau", "Gratin de chou-fleur", "Potée auvergnate"
 ];
 
 function genererMenu() {
